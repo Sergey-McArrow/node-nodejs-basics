a75c53f3935074224549fe9676c9f330734f062d
Sergey-McArrow <sergey.mcarrow@gmail.com>
2022-05-31 19:30:38 +0400

    feat: finish create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,21 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(path.join(__dirname, "files", "fresh.txt"));
 export const create = async () => {
-    // Write your code here 
+  fs.writeFile(
+    path.join(__dirname, "files", "fresh.txt"),
+    "I am fresh and young",
+    err => {
+      if (err) {
+        console.error("FS operation failed");
+      }
+      console.log("file written to " + path.join(__dirname, "files"));
+    }
+  );
 };
+
+create();
