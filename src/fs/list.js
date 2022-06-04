4f55581df7245c7ee54f6dcfe186fcfbf34d7de5
Sergey-McArrow <sergey.mcarrow@gmail.com>
2022-06-04 14:49:59 +0400

    feat: list finished

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,18 @@
-export const list = async () => {
-    // Write your code here 
+import { readdir } from "node:fs/promises";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pathToDir = path.join(__dirname, "files");
+
+export const list = async path => {
+  try {
+    const files = await readdir(path);
+    for (const file of files) console.log(file);
+  } catch (err) {
+    console.error(err);
+  }
 };
+list(pathToDir);
