8bb51888d93bd3b146bca909fc2bdd16d19dc763
Sergey-McArrow <sergey.mcarrow@gmail.com>
2022-06-04 14:41:18 +0400

    feat: delete finished

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,24 @@
-export const remove = async () => {
-    // Write your code here 
+import { unlink } from "node:fs/promises";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const FILE = "fileToRemove.txt";
+const pathToFile = path.join(__dirname, "files", FILE);
+
+export const remove = async filePath => {
+  try {
+    await unlink(filePath);
+    console.log(`${FILE} was removed`);
+  } catch (e) {
+    if (e.code === "ENOENT") {
+      console.log("FS operation failed");
+      console.log(e.code);
+    } else {
+      console.log(e.message);
+    }
+  }
 };
+remove(pathToFile);
