983ad2100347f446f2ed68f67ecf5da0e4a738e9
Sergey-McArrow <sergey.mcarrow@gmail.com>
2022-06-05 11:43:41 +0400

    feat: decompress finished

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,22 @@
-export const decompress = async () => {
-    // Write your code here 
+import { createUnzip, constants } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const FILE = "fileToDecompress.txt";
+const ZIP = "archive.gz";
+const dest = path.join(__dirname, "files", FILE);
+const src = path.join(__dirname, "files", ZIP);
+
+export const decompress = async (from, to) => {
+  const pipe = promisify(pipeline);
+  const gzip = createUnzip();
+  const source = createReadStream(from);
+  const destination = createWriteStream(to);
+  await pipe(source, gzip, destination);
 };
+decompress(src, dest);
